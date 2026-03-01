6411dc4315471c0380dc660264b5fa7fbcf93b1f
Lanovska-Sofiia10 <lanovskasofia@gmail.com>
2026-03-01 11:50:40 +0200

    add HW4

---
+++ HW/HW-4/HW4-1/main.js
@@ -0,0 +1,10 @@
+function exchange(sumUAH,currencyValues,exchangeCurrency){
+    for ( let currencys of currencyValues){
+        if(currencys.currency === exchangeCurrency){
+            let sum = sumUAH/currencys.value;
+            console.log(sum);
+            return sum;
+        }
+    }
+}
+exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD');
